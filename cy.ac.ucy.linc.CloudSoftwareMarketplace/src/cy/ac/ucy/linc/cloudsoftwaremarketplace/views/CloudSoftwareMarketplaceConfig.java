808aacbdd8276c5e9a8922d32de13c604a5c5b05
atryfonos <atryfo03@cs.ucy.ac.cy>
2016-03-26 20:18:15 +0200

    Improving repository configuration
    Signed-off-by: atryfonos <atryfo03@cs.ucy.ac.cy>

--- cy.ac.ucy.linc.CloudSoftwareMarketplace/src/cy/ac/ucy/linc/cloudsoftwaremarketplace/views/CloudSoftwareMarketplaceConfig.java
+++ cy.ac.ucy.linc.CloudSoftwareMarketplace/src/cy/ac/ucy/linc/cloudsoftwaremarketplace/views/CloudSoftwareMarketplaceConfig.java
@@ -8,10 +8,17 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Label;
 
 import cy.ac.ucy.linc.CloudSoftwareRepo.CloudSoftwareRepo;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class CloudSoftwareMarketplaceConfig {
 
-	protected Shell shell;
+	protected Shell shlConfigureRepository;
+	private Text txtRepo;
+	private Text txtUsername;
+	private Text txtPassword;
 
 	/**
 	 * Launch the application.
@@ -32,9 +39,9 @@ public class CloudSoftwareMarketplaceConfig {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlConfigureRepository.open();
+		shlConfigureRepository.layout();
+		while (!shlConfigureRepository.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -45,12 +52,51 @@ public class CloudSoftwareMarketplaceConfig {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shell = new Shell();
-		shell.setImage(SWTResourceManager.getImage(CloudSoftwareMarketplaceConfig.class, "/icons/settings.png"));
-		shell.setSize(450, 300);
+		shlConfigureRepository = new Shell(SWT.CLOSE | SWT.TITLE);
+		shlConfigureRepository.setText("Configure Repository");
+		shlConfigureRepository.setImage(SWTResourceManager.getImage(CloudSoftwareMarketplaceConfig.class, "/icons/settings.png"));
+		shlConfigureRepository.setSize(450, 300);
 		
-		Composite composite = new Composite(shell, SWT.BORDER);
-		composite.setBounds(10, 10, 414, 64);
+		
+		Composite composite = new Composite(shlConfigureRepository, SWT.BORDER);
+		composite.setBounds(10, 10, 414, 199);
+		
+		Label lblRepositoryUrl = new Label(composite, SWT.NONE);
+		lblRepositoryUrl.setBounds(10, 10, 98, 15);
+		lblRepositoryUrl.setText("Repository URL:");
+		
+		txtRepo = new Text(composite, SWT.BORDER);
+		txtRepo.setBounds(114, 7, 286, 21);
+		
+		Button btnPing = new Button(composite, SWT.NONE);
+		
+		btnPing.setImage(SWTResourceManager.getImage(CloudSoftwareMarketplaceConfig.class, "/icons/ping.png"));
+		btnPing.setBounds(130, 63, 135, 25);
+		btnPing.setText("Ping Repository");
+		
+		Label lblRepositoryAdminUsername = new Label(composite, SWT.NONE);
+		lblRepositoryAdminUsername.setBounds(10, 128, 160, 15);
+		lblRepositoryAdminUsername.setText("Repository Admin Username");
+		
+		txtUsername = new Text(composite, SWT.BORDER);
+		txtUsername.setBounds(174, 128, 226, 21);
+		
+		Label lblRepositoryAdminPassword = new Label(composite, SWT.NONE);
+		lblRepositoryAdminPassword.setText("Repository Admin Password");
+		lblRepositoryAdminPassword.setBounds(10, 170, 160, 15);
+		
+		txtPassword = new Text(composite, SWT.BORDER | SWT.PASSWORD);
+		txtPassword.setBounds(174, 164, 226, 21);
+		
+		Button btnOK = new Button(shlConfigureRepository, SWT.NONE);
+		btnOK.setImage(SWTResourceManager.getImage(CloudSoftwareMarketplaceConfig.class, "/icons/ok.png"));
+		btnOK.setBounds(81, 236, 111, 25);
+		btnOK.setText("OK");
+		
+		Button btnCancel = new Button(shlConfigureRepository, SWT.NONE);
+		btnCancel.setText("Cancel");
+		btnCancel.setImage(SWTResourceManager.getImage(CloudSoftwareMarketplaceConfig.class, "/icons/cancel.png"));
+		btnCancel.setBounds(259, 236, 111, 25);
 		CloudSoftwareRepo csr = new CloudSoftwareRepo();
 
 	}
