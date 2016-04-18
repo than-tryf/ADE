e2d0b7835922b130b690819ceb5cd5d9dcbe0a61
atryfonos <atryfo03@cs.ucy.ac.cy>
2016-04-18 20:31:28 +0300

    Created the layout for the local folder

--- cy.ac.ucy.linc.CloudSoftwareMarketplace/src/cy/ac/ucy/linc/cloudsoftwaremarketplace/views/CloudSoftwareMarketplaceLocal.java
+++ cy.ac.ucy.linc.CloudSoftwareMarketplace/src/cy/ac/ucy/linc/cloudsoftwaremarketplace/views/CloudSoftwareMarketplaceLocal.java
@@ -1,23 +1,82 @@
 package cy.ac.ucy.linc.cloudsoftwaremarketplace.views;
 
+import java.util.ArrayList;
+
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Tree;
+import org.eclipse.jface.viewers.DecoratingLabelProvider;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.IStyledLabelProvider;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.StyledString;
 import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.widgets.TreeColumn;
 import org.eclipse.jface.viewers.TreeViewerColumn;
+
+import cy.ac.ucy.linc.cloudsoftwaremarketplace.Activator;
+import cy.ac.ucy.linc.cloudsoftwaremarketplace.views.CloudSoftwareMarketplaceView.ViewContentProvider;
+import cy.ac.ucy.linc.cloudsoftwaremarketplace.views.CloudSoftwareMarketplaceView.ViewLabelProvider;
+
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.swt.dnd.DragSource;
+import org.eclipse.swt.dnd.DND;
 
 public class CloudSoftwareMarketplaceLocal extends ViewPart {
 
 	public static final String ID = "cy.ac.ucy.linc.cloudsoftwaremarketplace.views.CloudSoftwareMarketplaceLocal"; //$NON-NLS-1$
+	public ArrayList<String> result = new ArrayList<String>();
+	private Table table;
+	private TableViewer tableViewer;
 
-	private TreeViewer viewer;
+	class ViewContentProvider implements IStructuredContentProvider {
+		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
+		}
+
+		public void dispose() {
+		}
+
+		public Object[] getElements(Object parent) {
+			// return new String[] { "One", "Two", "Three", "Four", };
+			return result.toArray();
+		}
+	}
+
+	class ViewLabelProvider extends LabelProvider implements
+			ITableLabelProvider {
+		public String getColumnText(Object obj, int index) {
+			return getText(obj);
+		}
+
+		public Image getColumnImage(Object obj, int index) {
+			return getImage(obj);
+		}
+
+		public Image getImage(Object obj) {
+
+			// return PlatformUI.getWorkbench().getSharedImages()
+			// .getImage(ISharedImages.IMG_OBJ_ELEMENT);
+
+			final Image image = Activator.getImageDescriptor(
+					"icons/artifact.png").createImage();
+			return image;
+
+		}
+	}
 
 	public CloudSoftwareMarketplaceLocal() {
+
 	}
 
 	/**
@@ -27,13 +86,28 @@ public class CloudSoftwareMarketplaceLocal extends ViewPart {
 	 */
 	@Override
 	public void createPartControl(Composite parent) {
-		this.viewer = new TreeViewer( parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL );
-		Tree tree = this.viewer.getTree();
-		tree.setHeaderVisible(true);
-		createTreeColumns(tree);
+
+		tableViewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL
+				| SWT.V_SCROLL);
+		table = tableViewer.getTable();
+		table.setHeaderVisible(true);
+		{
+			TableViewerColumn tableViewerColumn = new TableViewerColumn(
+					tableViewer, SWT.NONE);
+			TableColumn tblclmnArtifactName = tableViewerColumn.getColumn();
+			tblclmnArtifactName.setWidth(272);
+			tblclmnArtifactName.setText("Artifact Name");
+		}
+		{
+			DragSource dragSource = new DragSource(table, DND.DROP_MOVE);
+		}
 		createActions();
 		initializeToolBar();
 		initializeMenu();
+		result.add("MySQL");
+		tableViewer.setContentProvider(new ViewContentProvider());
+		tableViewer.setLabelProvider(new ViewLabelProvider());
+		tableViewer.setInput(result);
 	}
 
 	/**
@@ -66,40 +140,31 @@ public class CloudSoftwareMarketplaceLocal extends ViewPart {
 
 	private void createTreeColumns(Tree tree) {
 
-		TreeColumn artName = new TreeColumn(tree, SWT.NONE);
-		artName.setText("Artifact Name"); //$NON-NLS-1$
-		artName.setAlignment(SWT.LEFT);
-		artName.setWidth(300);
-
-		/*TreeColumn deplStatus = new TreeColumn(tree, SWT.NONE);
-		deplStatus.setText("Status"); //$NON-NLS-1$
-		deplStatus.setAlignment(SWT.CENTER);
-		deplStatus.setWidth(100);
-
-		TreeColumn instanceID = new TreeColumn(tree, SWT.NONE);
-		instanceID.setText("Instance ID"); //$NON-NLS-1$
-		instanceID.setAlignment(SWT.CENTER);
-		instanceID.setWidth(300);
-
-		TreeColumn deplIP = new TreeColumn(tree, SWT.NONE);
-		deplIP.setText("IP Address"); //$NON-NLS-1$
-		deplIP.setAlignment(SWT.CENTER);
-		deplIP.setWidth(100);
-
-		TreeColumn imageID = new TreeColumn(tree, SWT.NONE);
-		imageID.setText("Image ID"); //$NON-NLS-1$
-		imageID.setAlignment(SWT.CENTER);
-		imageID.setWidth(100);
-
-		TreeColumn flavorID = new TreeColumn(tree, SWT.NONE);
-		flavorID.setText("Flavor ID"); //$NON-NLS-1$
-		flavorID.setAlignment(SWT.CENTER);
-		flavorID.setWidth(100);
-
-		TreeColumn keyPair = new TreeColumn(tree, SWT.NONE);
-		keyPair.setText("Key Pair"); //$NON-NLS-1$
-		keyPair.setAlignment(SWT.CENTER);
-		keyPair.setWidth(100);*/
+		/*
+		 * TreeColumn deplStatus = new TreeColumn(tree, SWT.NONE);
+		 * deplStatus.setText("Status"); //$NON-NLS-1$
+		 * deplStatus.setAlignment(SWT.CENTER); deplStatus.setWidth(100);
+		 * 
+		 * TreeColumn instanceID = new TreeColumn(tree, SWT.NONE);
+		 * instanceID.setText("Instance ID"); //$NON-NLS-1$
+		 * instanceID.setAlignment(SWT.CENTER); instanceID.setWidth(300);
+		 * 
+		 * TreeColumn deplIP = new TreeColumn(tree, SWT.NONE);
+		 * deplIP.setText("IP Address"); //$NON-NLS-1$
+		 * deplIP.setAlignment(SWT.CENTER); deplIP.setWidth(100);
+		 * 
+		 * TreeColumn imageID = new TreeColumn(tree, SWT.NONE);
+		 * imageID.setText("Image ID"); //$NON-NLS-1$
+		 * imageID.setAlignment(SWT.CENTER); imageID.setWidth(100);
+		 * 
+		 * TreeColumn flavorID = new TreeColumn(tree, SWT.NONE);
+		 * flavorID.setText("Flavor ID"); //$NON-NLS-1$
+		 * flavorID.setAlignment(SWT.CENTER); flavorID.setWidth(100);
+		 * 
+		 * TreeColumn keyPair = new TreeColumn(tree, SWT.NONE);
+		 * keyPair.setText("Key Pair"); //$NON-NLS-1$
+		 * keyPair.setAlignment(SWT.CENTER); keyPair.setWidth(100);
+		 */
 	}
 
 }
